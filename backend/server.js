aeb2afd9ab9a45e86c895a87558d046a5ff13643
RishiSamineni <rsaminen@gitam.in>
2025-07-18 20:50:59 +0530

    Updated the Routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const incomeRoutes = require("./routes/IncomeRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
